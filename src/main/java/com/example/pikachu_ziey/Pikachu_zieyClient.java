4c3eb33425c1e114e3bee469cf031cbc01050528
PIAKCHU <15366511+piakchu@user.noreply.gitee.com>
2025-08-17 21:07:36 +0800

    初始化

---
+++ src/main/java/com/example/pikachu_ziey/Pikachu_zieyClient.java
@@ -0,0 +1,11 @@
+package com.example.pikachu_ziey;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Pikachu_zieyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
